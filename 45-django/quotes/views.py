ffcc3fc088700f8a4693e3b3ba6a6c415890073a
Rob Ward <wardrobert@gmail.com>
2019-05-29 12:02:15 -0400

    the hard way to get a pk

--- 45-django/quotes/views.py
+++ 45-django/quotes/views.py
@@ -10,7 +10,10 @@ def quote_list(request):
     return render(request, 'quotes/quote_list.html', {'quotes': quotes})
 
 def quote_detail(request):
-    pass
+    try:
+        Quote.objects.get(pk=pk)
+    except Quote.DoesNotExist:
+        raise Http404
 
 
 def quote_new(request, pk):
