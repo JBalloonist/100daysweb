dc7ae24ea79d5f0bd9586fc303c979faa896320c
Rob Ward <wardrobert@gmail.com>
2019-06-04 14:58:24 -0400

    update static file settings for azure

--- 45-django/mysite/settings.py
+++ 45-django/mysite/settings.py
@@ -120,13 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-# STATIC_URL = '/static/'
-
-# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_ROOT, 'static'),
-# )
+STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
